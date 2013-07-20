7840dcf82aad77a67fdc3ef61c7b7150164bb05b
meotimdihia <meotimdihia@gmail.com>
2013-07-20 08:56:10 +0700

    updated

--- install.sh
+++ install.sh
@@ -1,1 +1,26 @@
-#curl -Lo- https://bit.ly/janus-bootstrap | bash
+#!/usr/bin/env bash
+
+! [ -f $HOME/.bash_profile.bak ] && \
+	cp $HOME/.bash_profile $HOME/.bash_profile.bak && \
+	echo 'Backed up bash_profile to bash_profile.bak'
+
+rm $HOME/.bash_profile
+ln -s $HOME/.dotfiles/bash_profile $HOME/.bash_profile
+
+for file in $HOME/.dotfiles/config/*
+do
+	basefile=`basename $file`
+	if [ -f "$HOME/.$basefile" ];
+	then
+		! [ -f "$HOME/.$basefile.bak" ] && \
+			cp "$HOME/.$basefile" "$HOME/.$basefile.bak" && \
+			echo "Backed up existing .$basefile to .$basefile.bak"
+	fi
+	if ! [ "$file" == '.' ] && ! [ "$file" == '..' ]; then
+		rm "$HOME/.$basefile"
+		ln -s $file "$HOME/.$basefile"
+		echo " - Linked $basefile"
+	fi
+done
+
+curl -Lo- https://bit.ly/janus-bootstrap | bash
